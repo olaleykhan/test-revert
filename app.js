1eef95da5fb15e44e9b6633fa7d6f6854c8b76ec
olaleykhan <realjoy9@gmail.com>
2022-06-22 11:47:14 +0100

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+<Grid container direction="column" alignItems="center">
+{/* <Grid item container justifyContent={showClose ? 'space-between' : 'flex-start'} className={classes.header}>
+  <Grid item container alignItems="center" className={classes.hello}>
+    <Trans
+      i18nKey="hello.there"
+      defaults="Hello, There!<hand/>"
+      components={{ hand: <img className={classes.hand} src={img} alt="hand" /> }}
+    />
+  </Grid>
+  {showClose && (
+    <Grid item onClick={() => UiStore.close()} className={classes.exit}>
+      <CloseIcon className={classes.close} />
+    </Grid>
+  )}
+</Grid> */}
+{/* <Grid item className={classes.topText}>
+  {ThemeStore.theme.onboarding?.landing?.message
+    && (
+      <Trans
+        i18nKey=""
+        defaults={useTranslationValue(ThemeStore.theme.onboarding.landing.message, i18n)
+          .replace('{{name}}', '<brand>{{name}}</brand>')}
+        values={{
+          name: AppStore.app.name,
+          value: formatCash(freePoints),
+        }}
+        components={{
+          brand: <span className={classes.branded} />,
+        }}
+      />
+    )}
+</Grid>
+<Grid item className={classes.image} /> */}
+</Grid>
