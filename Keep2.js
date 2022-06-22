43c9f4a48f4f89c0e281207c7c0e055db36f812c
olaleykhan <realjoy9@gmail.com>
2022-06-22 11:50:17 +0100

    keep1 &2

---
+++ Keep2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Keep2 = () => {
+  return (
+    <div>Keep2</div>
+  )
+}
+
+export default Keep2
---
+++ keep1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const keep1 = () => {
+  return (
+    <div>keep1</div>
+  )
+}
+
+export default keep1
